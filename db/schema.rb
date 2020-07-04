751971fd9185b00f44cd18eed2839a1a7f861e70
Abdoulaye-Thespy <njigouhrazak@iut-dhaka.edu>
2020-07-04 10:38:10 +0100

    Added event model along with association on event creator

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_02_091815) do
+ActiveRecord::Schema.define(version: 2020_07_04_044321) do
+
+  create_table "events", force: :cascade do |t|
+    t.date "date"
+    t.string "name_event"
+    t.text "description"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
