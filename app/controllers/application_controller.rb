a1580b31964c39af1fbee16b1a5807cba91f4d9e
Abdoulaye-Thespy <njigouhrazak@iut-dhaka.edu>
2020-07-04 05:40:44 +0100

    User sign in set up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,11 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user 
-   def current_user
-    if session[:user_id]
-      @current_user ||= User.find(session[:user_id])
-    else
-      @current_user = nil
-    end
-  end
+helper_method :current_user
+helper_method :logged_in?
+def current_user   
+	User.find_by(id: session[:id])
+end
+def logged_in?
+       
+    !current_user.nil?
+end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,6 +8,7 @@ class SessionsController < ApplicationController
      if user
       session[:id] = user.id
       redirect_to new_session_path, notice: "Logged in!"
+
     else
       flash.now[:alert] = "Email or password is invalid"
        redirect_to new_session_path
