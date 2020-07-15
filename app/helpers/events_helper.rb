fdd02c99d9b743a372657974fc42380f8571b13f
Abdoulaye-Thespy <njigouhrazak@iut-dhaka.edu>
2020-07-15 10:25:59 +0100

    removed logics in index view

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,2 +1,8 @@
 module EventsHelper
+
+	def attend(y)
+      if logged_in?
+     link_to 'Attend', new_user_event_attended_event_path(current_user.id, y), class:"card-footer-item", data: {confirm: "Attend to this event?", disable_with: "Attending..." }
+    end
+	end
 end
